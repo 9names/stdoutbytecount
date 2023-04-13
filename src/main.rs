2ab6342d9db81e9a4f6997a348340253e061f1d3
9names <60134748+9names@users.noreply.github.com>
2023-04-13 19:00:26 +1000

    Add quick+dirty averaging

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,28 @@
+use std::collections::VecDeque;
 use std::io::Read;
 use std::time::Instant;
 
 fn main() {
-    let mut bytes_read = 0;
     let mut bytes_read_instant = 0;
     let start = Instant::now();
     let mut last_secs = 0;
     let mut input = std::io::stdin();
     let mut buffer: [u8; 1024] = [0; 1024];
+    let mut values = VecDeque::<u64>::with_capacity(10);
     loop {
         match input.read(&mut buffer) {
             Ok(size) => {
-                let _read = &buffer[0..size];
-                bytes_read += size;
                 bytes_read_instant += size;
                 let elapsed_time = start.elapsed().as_secs();
                 if elapsed_time != last_secs {
                     last_secs = elapsed_time;
-                    // TODO: windowed averaging to avoid initial error
-                    println!(
-                        "Bytes/sec: {}, average: {}",
-                        bytes_read_instant,
-                        bytes_read / elapsed_time as usize
-                    );
+                    values.push_front(bytes_read_instant as u64);
 
+                    if values.len() > 10 {
+                        values.pop_back();
+                    }
+                    let average: u64 = values.iter().sum::<u64>() / values.len() as u64;
+                    println!("Bytes/sec: {}, average: {}", bytes_read_instant, average);
                     bytes_read_instant = 0;
                 }
             }
